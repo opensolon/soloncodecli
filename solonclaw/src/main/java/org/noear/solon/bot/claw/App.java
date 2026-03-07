1af86716285fdaaed90fb0e8fd55f06651b94548
noear <noear@live.cn>
2026-03-07 19:48:09 +0800

    调整 代码结构

---
+++ solonclaw/src/main/java/org/noear/solon/bot/claw/App.java
@@ -0,0 +1,14 @@
+package org.noear.solon.bot.claw;
+
+import org.noear.solon.Solon;
+
+/**
+ *
+ * @author noear 2026/3/7 created
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        Solon.start(App.class, args);
+    }
+}
---
+++ solonclaw/src/main/java/org/noear/solon/bot/claw/portal/WebSocketGate.java
@@ -0,0 +1,9 @@
+package org.noear.solon.bot.claw.portal;
+
+/**
+ *
+ * @author noear 2026/3/7 created
+ *
+ */
+public class WebSocketGate {
+}
