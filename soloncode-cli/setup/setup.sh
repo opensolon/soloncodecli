c0d703df94dce005a7db100c84e2a0c810883917
noear <noear@live.cn>
2026-04-26 22:47:16 +0800

    v2026.4.29

--- soloncode-cli/setup/setup.sh
+++ soloncode-cli/setup/setup.sh
@@ -6,7 +6,7 @@
 
 set -e
 
-VERSION="v2026.4.28"
+VERSION="v2026.4.29"
 PACKAGE_URL="https://gitee.com/opensolon/soloncode/releases/download/${VERSION}/soloncode-cli-bin-${VERSION}.tar.gz"
 TEMP_DIR="/tmp/soloncode-install"
 
